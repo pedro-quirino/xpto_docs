e630257583167d51cdfc8fe3e19faedb578b085e
Pedro Gomes Quirino <pedro.quirino@conductor.com.br>
2020-02-05 12:03:57 -0300

    Updated master branch of deployment repo to point to latest head of submodules

--- git-update-submodules.sh
+++ git-update-submodules.sh
@@ -4,15 +4,16 @@ BRANCH=$1
 shift
 
 if [ -z $BRANCH ]; then
-  echo "Missing argument (branch name)";
+  echo "Missing 2nd argument (branch name)";
   exit 1;
 fi
 
-git checkout $BRANCH
+git checkout $BRANCH && git pull --ff origin $BRANCH
 
 git submodule sync
 git submodule init
-git submodule update --init --recursive
+git submodule update
+git submodule foreach "(git checkout $BRANCH && git pull --ff origin $BRANCH && git push origin $BRANCH) || true"
 
 for i in $(git submodule foreach --quiet 'echo $path')
 do
@@ -22,5 +23,3 @@ done
 
 git commit -m "Updated $BRANCH branch of deployment repo to point to latest head of submodules"
 git push origin $BRANCH
-
-
