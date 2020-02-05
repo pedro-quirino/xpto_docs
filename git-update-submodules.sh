517a49516be9d9a7715183b658b15ca1245e0690
Pedro Gomes Quirino <pedro.quirino@conductor.com.br>
2020-02-05 11:38:21 -0300

    Updated master branch of deployment repo to point to latest head of submodules

--- git-update-submodules.sh
+++ git-update-submodules.sh
@@ -14,11 +14,11 @@ git submodule sync
 git submodule init
 git submodule update --init --recursive
 
-for i in $(git submodule foreach --quiet 'echo $path')
-do
-  echo "Adding $i to root repo"
-  git add "$i"
-done
+# for i in $(git submodule foreach --quiet 'echo $path')
+# do
+  # echo "Adding $i to root repo"
+  # git add "$i"
+# done
 
 git add .
 git commit -m "Updated $BRANCH branch of deployment repo to point to latest head of submodules"
